2ec4536415c396cbc931c5e20ec9b2ed970e6af4
Tianxiang DU <dutianxiang0220@gmail.com>
2019-06-13 17:15:17 +0800

    Add commits

--- baidubaike/html_downloader.py
+++ baidubaike/html_downloader.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-  
 import urllib.request
 
-class HtmlDownloader(object):
+class HtmlDownloader(object): # To downloader the urls.
     def download(self, url):
         if url is None:
             return None
         response = urllib.request.urlopen(url)
-        if response.getcode() != 200:
+        if response.getcode() != 200: # 200 means that it is successfully connected.
             return None
         return response.read()
